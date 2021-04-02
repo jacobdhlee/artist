1d294cecd4e9beb070b8d305c334ba1e34dba36e
jacobdhlee <jacobdhlee@gmail.com>
2021-04-02 16:55:15 -0400

    add show and migrate also add button on venues page

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -69,6 +69,18 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime())
+
+  # foreign key relation
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+
+  artist = db.relationship("Artist", backref=db.backref('Show', cascade='all, delete'))
+  venue = db.relationship("Venue", backref=db.backref('Show', cascade='all, delete'))
+
 
 #----------------------------------------------------------------------------#
 # Filters.
